2ea48397e440bdc0fefb7ff27a553778a79fd469
Michael Schwarz <michi.schwarz@gmail.com>
2019-07-30 22:45:19 +0200

    Fix data files not being installed

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setuptools.setup(
     name='inkscape-shape-cli',
     version='1.0.0',
     packages=setuptools.find_packages(),
+    package_data={'': ['*.txt']},
     entry_points=dict(
         console_scripts=[
             'inkscape2dxf = inkscapeshapecli.inkscape2dxf:script_main']),
