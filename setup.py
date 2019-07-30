13e107727d59b2dd5f969573a70f22ecd30d6be8
Michael Schwarz <michi.schwarz@gmail.com>
2019-07-30 15:22:39 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+
+setuptools.setup(
+    name='inkscape-shape-cli',
+    version='1.0.0',
+    packages=['inkscapeshapecli'],
+    entry_points=dict(
+        console_scripts=[]),
+    install_requires=[])
